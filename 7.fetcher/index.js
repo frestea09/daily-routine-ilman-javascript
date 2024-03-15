b3bb0f4a8284dc44f01e7dfceb3e7d50df56cfc6
ilman <ilman.teguh@photon.com>
2024-03-15 09:06:56 +0700

    this is latihan 13 maret

--- 7.fetcher/index.js
+++ 7.fetcher/index.js
@@ -1,9 +1,9 @@
 const urlSaya = "https://jsonplaceholder.typicode.com/users";
 
-const getData = async (urlSaya) => {
-  const response = await fetch(urlSaya);
+const getData = async (inputUrl) => {
+  const response = await fetch(inputUrl);
   const data = await response.json();
-  console.log(data);
+  console.log("data", data);
 };
 
 getData(urlSaya);
