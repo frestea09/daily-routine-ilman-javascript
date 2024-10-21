c37afc638a597b5b487789dff9768a4e482235b6
frestea09 <ilmanfrasetya@gmail.com>
2024-10-21 14:25:34 +0700

    prepare : get ready for train git

--- src/object/index.ts
+++ src/object/index.ts
@@ -1,11 +1,7 @@
-const obejctTemp = {
-  nama: "ilman",
-  umur: 23,
-  bilanganPertama: 3,
-  bilanganKedua: 3,
+const objectTemp = {
+  name: "ilman",
+  age: 30,
 };
 
-console.log(obejctTemp.nama);
-
-const { nama, umur, bilanganPertama: ganteng } = obejctTemp;
-console.log(nama, umur, ganteng);
+const { age, name } = objectTemp;
+console.log(age, name);
